0dbb683711de2f6cbbdc25b00009d9213e4b0a8a
kyaloeriq <kyaloeriq@gmail.com>
2024-01-17 04:34:09 -0800

    push instruction code

--- ic_push.c
+++ ic_push.c
@@ -9,4 +9,15 @@ void push(stack_t **stack, unsigned int line_num)
 		fprintf(stderr, "L%u: usage: push integer\n", line_num);
 		exit(EXIT_FAILURE);
 	}
-}
+	/*Creating a new stack node and setting value*/
+	stack_t *new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	new_node->n = value;
+	new_node->next = *stack; /*New node's next pointer to the current top*/
+	*stack = new_node; /*Update the stack's top to the new node*/
+
+
