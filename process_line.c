e9987ea33d67708371f603fc6b9d5457285a76dc
kyaloeriq <kyaloeriq@gmail.com>
2024-01-17 12:44:50 -0800

    processes each line and executes operation

---
+++ process_line.c
@@ -0,0 +1,32 @@
+#include "monty.h"
+/**
+ * process_line - Processes line and executes operation
+ * @line: line
+ * @stack: pinter to stack
+ * @line_num: number where instruction appears
+ */
+void process_line(char *line, stack_t **stack, unsigned int line_num)
+{
+	char *opcode, *arg;
+
+	/*Tokenizes line to extract opcode and argument*/
+	opcode = strtok(line, " \t\n");
+	arg = strtok(NULL, " \t\n");
+
+	if (opcode != NULL)
+	{
+		if (strcmp(opcode, "push") == 0)
+		{
+			push(stack, line_num);
+		}
+		else if (strcmp(opcode, "pall") == 0)
+		{
+			pall(stack, line_num);
+		}
+		else
+		{ /*Handles unknown opcode*/
+			fprintf(stderr, "Error: Unknown opcode %s\n", opcode);
+			exit(EXIT_FAILURE);
+		}
+	}
+}
