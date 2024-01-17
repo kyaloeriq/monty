d1bd859a57d5e9ac79bdee759ebaa45e6d42c761
kyaloeriq <kyaloeriq@gmail.com>
2024-01-17 03:25:59 -0800

    push instruction code

---
+++ ic_push.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+void push(stack_t **stack, unsigned int line_num)
+{
+	/*Parsing of argument from the line*/
+	int value;
+	if (value == NULL)
+	{
+		fprintf(stderr, "L<line_number>: usage: push integer\n", line_num);
+		exit(EXIT_FAILURE);
+	}
+}
