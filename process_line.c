784b721c05456430f34c9a7159dfb3d6fab181e2
kyaloeriq <kyaloeriq@gmail.com>
2024-01-18 00:34:09 -0800

    processes each line and executes operation

--- process_line.c
+++ process_line.c
@@ -17,51 +17,32 @@ void process_line(char line[1024], stack_t **stack, unsigned int line_num)
 	opcode = strtok(line, " \t\n");
 	arg = strtok(NULL, " \t\n");
 
-	if (opcode != NULL)
+	if (!opcode)
+		return;
+	if (strcmp(opcode, "push") == 0)
 	{
-		if (strcmp(opcode, "push") == 0)
+		if (!arg || (!(value = atoi(arg)) && strcmp(arg, "0") != 0))
 		{
-			if (arg == NULL)
-			{
-				fprintf(stderr, "L%u: usage: push integer\n", line_num);
-				exit(EXIT_FAILURE);
-			}
-			value = atoi(arg); /*Convert argument to an integer*/
-			if (value == 0 && strcmp(arg, "0") != 0)
-			{
-				fprintf(stderr, "L%u: usage: push integer\n", line_num);
-				exit(EXIT_FAILURE);
-			} /*Call the push function*/
-			push(stack, line_num, value);
-		}
-		else if (strcmp(opcode, "pall") == 0)
-		{
-			pall(stack, line_num);
-		}
-		else if (strcmp(opcode, "pint") == 0)
-		{
-			pint(stack, line_num);
-		}
-		else if (strcmp(opcode, "pop") == 0)
-		{
-			pop(stack, line_num);
-		}
-		else if (strcmp(opcode, "swap") == 0)
-		{
-			swap(stack, line_num);
-		}
-		else if (strcmp(opcode, "add") == 0)
-		{
-			add(stack, line_num);
-		}
-		else if (strcmp(opcode, "nop") == 0)
-		{
-			nop(stack, line_num);
-		}
-		else
-		{ /*Handles unknown opcode*/
-			fprintf(stderr, "Error: Unknown opcode %s\n", opcode);
+			fprintf(stderr, "L%u: usage: push integer\n", line_num);
 			exit(EXIT_FAILURE);
 		}
+		push(stack, line_num, value);
+	}
+	else if (strcmp(opcode, "pall") == 0)
+		pall(stack, line_num);
+	else if (strcmp(opcode, "pint") == 0)
+		pint(stack, line_num);
+	else if (strcmp(opcode, "pop") == 0)
+		pop(stack, line_num);
+	else if (strcmp(opcode, "swap") == 0)
+		swap(stack, line_num);
+	else if (strcmp(opcode, "add") == 0)
+		add(stack, line_num);
+	else if (strcmp(opcode, "nop") == 0)
+		nop(stack, line_num);
+	else
+	{ /*Handles unknown opcode*/
+		fprintf(stderr, "Error: Unknown opcode %s\n", opcode);
+		exit(EXIT_FAILURE);
 	}
 }
