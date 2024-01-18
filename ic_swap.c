09accbefd5e459bbff7a77c13ff39af29d6f1510
kyaloeriq <kyaloeriq@gmail.com>
2024-01-17 22:58:32 -0800

    swap instruction code

---
+++ ic_swap.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+/**
+ * swap - swaps the top two elements of the stack
+ * @stack: pointer to stack
+ * @line_num: number of line
+ */
+void swap(stack_t **stack, unsigned int line_num)
+{
+	int temp;
+
+	if (!*stack || !(*stack)->next)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_num);
+		exit(EXIT_FAILURE);
+	}
+	temp = (*stack)->n;
+	(*stack)->n = (*stack)->next->n;
+	(*stack)->next->n  = temp;
+}	
