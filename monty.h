4bfb0aac92dfae348dfe030a9b86c0bc6acf594c
kyaloeriq <kyaloeriq@gmail.com>
2024-01-18 12:51:34 -0800

    header file

--- monty.h
+++ monty.h
@@ -45,5 +45,6 @@ void pop(stack_t **stack, unsigned int line_num);
 void swap(stack_t **stack, unsigned int line_num);
 void add(stack_t **stack, unsigned int line_num);
 void nop(stack_t **stack, unsigned int line_num);
+void sub(stack_t **stack, unsigned int line_num);
 
 #endif
