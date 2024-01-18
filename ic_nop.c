6d67661658e1e18e08040fb5fc8a7ca172c17e35
kyaloeriq <kyaloeriq@gmail.com>
2024-01-17 23:33:19 -0800

    doesn’t do anything

---
+++ ic_nop.c
@@ -0,0 +1,11 @@
+#include "monty.h"
+/**
+ * nop - doesn’t do anything
+ * @stack: pointer to stack
+ * @line_num: number of line
+ */
+void nop(stack_t **stack, unsigned int line_num)
+{
+	(void)stack;
+	(void)line_num;
+}
