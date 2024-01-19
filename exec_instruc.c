0d4c6ba0aea15726abb7121f43e8a3cfd4e3d2c4
kyaloeriq <kyaloeriq@gmail.com>
2024-01-19 03:44:10 -0800

    execution of instructions

--- exec_instruc.c
+++ exec_instruc.c
@@ -45,6 +45,8 @@ void exec_instruc(instruction_t instr, stack_t **stack, unsigned int line_num)
 		sub(stack, line_num);
 	else if (strcmp(instr.opcode, "div") == 0)
 		monty_div(stack, line_num);
+	else if (strcmp(instr.opcode, "div") == 0)
+		monty_mul(stack, line_num);
 	else
 	{ /*Handles unknown opcode*/
 		fprintf(stderr, "L%u: unknown opcode %s\n", line_num, instr.opcode);
