8a5bd322a342f71fd2b439baea1c49d779ca381c
kyaloeriq <kyaloeriq@gmail.com>
2024-01-18 23:46:16 -0800

    div instruction code

---
+++ div.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+/**
+ * div - divides the second top element
+ * @stack: pointer to stack
+ * @line_num: number of line
+ */
+void div(stack_t **stack, unsigned int line_num)
+{
+	if (!*stack || !(*stack)->next)
+	{
+		fprintf(stderr, "L%d: can't div, stack too short\n", line_num);
+		exit(EXIT_FAILURE);
+	}
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_num);
+		exit(EXIT_FAILURE);
+	}
+	(*stack)->next->n /= (*stack)->n;
+	pop(stack, line_num);
+}
