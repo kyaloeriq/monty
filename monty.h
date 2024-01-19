460ada3324902f6e14e346b4a50bf9e2c232bded
kyaloeriq <kyaloeriq@gmail.com>
2024-01-18 23:59:37 -0800

    header file

--- monty.h
+++ monty.h
@@ -48,6 +48,6 @@ void swap(stack_t **stack, unsigned int line_num);
 void add(stack_t **stack, unsigned int line_num);
 void nop(stack_t **stack, unsigned int line_num);
 void sub(stack_t **stack, unsigned int line_num);
-void div(stack_t **stack, unsigned int line_num);
+void monty_div(stack_t **stack, unsigned int line_num);
 
 #endif
